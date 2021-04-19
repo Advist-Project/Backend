31bff5a1379e5d150d08264f3a680f4f4671b7bb
criel2019 <pjhk579700@gmail.com>
2021-04-19 16:10:35 +0900

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -15,6 +15,13 @@ if (!('error' in envFound)) {
 const mongo_user = process.env.MONGO_USERNAME
 const mongo_pass = process.env.MONGO_PASSWORD
 const mongo_host = process.env.MONGO_HOST
+const google_client = process.env.GOOGLE_CLIENT_ID;
+const google_secret = process.env.GOOGLE_CLIENT_SECRET;
+const kakao_client = process.env.KAKAO_CLIENT_ID;
+const kakao_secret = process.env.KAKAO_CLIENT_SECRET;
+const naver_client = process.env.NAVER_CLIENT_ID;
+const naver_secret = process.env.NAVER_CLIENT_SECRET;
+
 const MONGO_OPTIONS = {
     useNewUrlParser : true,    
     useFindAndModify : false,  
@@ -40,12 +47,26 @@ const server_host = process.env.SERVER_HOSTNAME;
 const SERVER = {
     hostname : server_host
 }
+const GOOGLE = {
+    client: google_client,
+    secret: google_secret
+}
+const KAKAO = {
+    client: kakao_client,
+    secret: kakao_secret
+}
+const NAVER = {
+    client: naver_client,
+    secret: naver_secret
+}
 
 config = {
     mongo : MONGO,
     server : SERVER,
-    bootpay : BOOTPAY
-
+    bootpay : BOOTPAY,
+    google : GOOGLE,
+    kakao: KAKAO,
+    naver: NAVER,
 }
 
 export default config;
