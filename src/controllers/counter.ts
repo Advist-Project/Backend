1b04bccc670feec6304a51bfaafefa84e6e9cb44
yuum <kangum99@naver.com>
2021-04-20 19:23:15 +0900

    promise -> async/await

--- src/controllers/counter.ts
+++ src/controllers/counter.ts
@@ -1,19 +1,18 @@
 import Counter from "../models/counter"
 
-export default function getNextSequence(id: string) {
-  return Counter.findOneAndUpdate(
-    { id: id },
-    { $inc: { seq: 1 } },
-    { new: true }
-  )
-    .then((result) => {
-      if (result instanceof Counter) {
-        console.log(result.seq)
-        return result.seq
-      }
-    })
-    .catch((error) => {
-      console.log(error.message)
-      return error.message
-    })
+export default async function getNextSequence(id: string) {
+  try {
+    const result = await Counter.findOneAndUpdate(
+      { id: id },
+      { $inc: { seq: 1 } },
+      { new: true }
+    )
+    if (result instanceof Counter) {
+      console.log(result.seq)
+      return result.seq
+    }
+  } catch (error) {
+    console.log(error.message)
+    return error.message
+  }
 }
