0e8c8e4e5e87519be6ec9f57d2bf09720420a610
yuum <kangum99@naver.com>
2021-05-16 18:07:12 +0900

    Update user.ts

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,13 +1,22 @@
 import express from "express"
+import userInfo from "../controllers/userInfo"
 var router = express.Router()
 module.exports = function (passport) {
 
-
+  // const isOkOnboarding = async () => {
+  //   try {
+  //     const user = await userInfo.userFindOne()
+  //   } catch (error) {
+  //     console.log("isOkOnboarding" + error.message)
+  //     return -1
+  //   }
+  // }
 
   router.get('/auth/google', passport.authenticate('google', { scope: ['email', 'profile'] }))
   router.get('/auth/google/callback',
     passport.authenticate('google', { failureRedirect: '/user/login', session: true }),
     function (req, res) {
+      console.log(res)
       res.redirect('https://www.advist.kr')
     })
   router.get('/auth/kakao', passport.authenticate('kakao'))
