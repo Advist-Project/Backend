576120fd7932349bfec03ae518a56f59d751f144
yuum <kangum99@naver.com>
2021-05-16 20:32:56 +0900

    Update user.ts

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,26 +3,36 @@ import userInfo from "../controllers/userInfo"
 var router = express.Router()
 module.exports = function (passport) {
 
-  // const isOkOnboarding = async () => {
-  //   try {
-  //     const user = await userInfo.userFindOne()
-  //   } catch (error) {
-  //     console.log("isOkOnboarding" + error.message)
-  //     return -1
-  //   }
-  // }
+  const toJson = (req, res) => {
+    const userJsonString = JSON.stringify(req.user)
+    const userJson = JSON.parse(userJsonString)
+    console.log(userJson.orderIds)
+    console.log(userJson.company)
+    console.log(userJson.email)
+    if (userJson.company === undefined || userJson.company === "")
+      return 0;
+    else
+      return 1;
+  }
 
   router.get('/auth/google', passport.authenticate('google', { scope: ['email', 'profile'] }))
   router.get('/auth/google/callback',
     passport.authenticate('google', { failureRedirect: '/user/login', session: true }),
     function (req, res) {
       console.log("req.user = " + req.user)
-      const userJsonString = JSON.stringify(req.user)
-      const userJson = JSON.parse(userJsonString)
-      console.log(userJson.orderIds)
-      console.log(userJson.company)
-      console.log(userJson.email)
-      res.redirect('https://www.advist.kr')
+      // const userJsonString = JSON.stringify(req.user)
+      // const userJson = JSON.parse(userJsonString)
+      // console.log(userJson.orderIds)
+      // console.log(userJson.company)
+      // console.log(userJson.email)
+      if (toJson(req, res) === 0) {
+        console.log("값이 없습니다")
+        res.redirect('https://www.advist.kr')
+      } else {
+        console.log("호호호")
+        res.redirect('https://www.advist.kr')
+      }
+
     })
   router.get('/auth/kakao', passport.authenticate('kakao'))
 
