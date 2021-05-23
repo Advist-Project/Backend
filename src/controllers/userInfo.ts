abfe8d5e1f6a56a9b43e9106e7f166899a3c1509
yuum <kangum99@naver.com>
2021-05-23 18:22:00 +0900

    필요 없는 코드 삭제

--- src/controllers/userInfo.ts
+++ src/controllers/userInfo.ts
@@ -24,35 +24,6 @@ const userFindUpdate = async (id: number, param: any) => {
         console.log("userFindUpdate" + error.message)
     }
 }
-// 로그인 온보딩 기존 값 보여주기-> 필요 없는듯..ㅎ
-// const getLoginOnboarding = async (req: Request, res: Response, next: NextFunction) => {
-//     try {
-//         const userId = parseInt(req.params.userId)
-//         const userInfo = await userFindOne(userId)
-//         if (userInfo === null || userInfo === undefined) {
-//             res.status(501).json({
-//                 message: "userId에 맞는 정보가 없습니다"
-//             })
-//         } else {
-//             const onboarding = {
-//                 "userId": userInfo["userId"] || "",
-//                 "realEmail": userInfo["realEmail"] || "",
-//                 "company": userInfo["company"] || "",
-//                 "jobDepartment": userInfo["jobDepartment"] || "",
-//                 "career": userInfo["career"] || ""
-//             }
-//             res.status(200).json({
-//                 result: onboarding
-//             })
-//         }
-
-//     }
-//     catch (error) {
-//         res.status(500).json({
-//             message: error.message
-//         })
-//     }
-// }
 
 // user에 온보딩 결과 값 넣기
 const postLoginOnboarding = async (req: Request, res: Response, next: NextFunction) => {
@@ -74,10 +45,10 @@ const postLoginOnboarding = async (req: Request, res: Response, next: NextFuncti
         })
     }
 }
+
 export default
     {
         userFindOne,
         userFindUpdate,
-        // getLoginOnboarding,
         postLoginOnboarding
     }
