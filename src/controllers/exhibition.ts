c4ff9e77dd27885a58d6895e18888dd26d259814
criel2019 <pjhk579700@gmail.com>
2021-07-21 13:06:00 +0900

    Admin용 레퍼런스 함수 추가

--- src/controllers/exhibition.ts
+++ src/controllers/exhibition.ts
@@ -139,7 +139,7 @@ const adminExhibitions = async (req: Request, res: Response, next: NextFunction)
         // iteminfo 붙이는 로직
         for (let i = 0; i < exhibition.length; i++) {
             const itemIdArray = exhibition[i]?.itemId
-            const Items: any = await referenceOfExhibition(itemIdArray)
+            const Items: any = await referenceOfExhibitionAdmin(itemIdArray)
             if (Items === -1) {
                 res.status(501).json({
                     error: "itemId에 오류가 있습니다."
@@ -163,6 +163,57 @@ const adminExhibitions = async (req: Request, res: Response, next: NextFunction)
     }
 }
 
+const referenceOfExhibitionAdmin = async (itemIds?: Array<number>) => {
+    try {
+        if (itemIds == undefined) return -1
+        else {
+            const infoPromise = itemIds.map(async function (itemId: number): Promise<object | null> {
+                const item = await Item.findOne({ itemId: itemId })
+                // 빈 값을 찾는다
+                if (_.isEmpty(item)) {
+                    // 안그러면 undefine으로 값이 채워짐.-> map이라서
+                    return {}
+                } else {
+                    const realOptions = await itemController.isOkOptions(item?.options, itemId)
+                    const itemInfo = {
+                        "itemId": item?.itemId,
+                        "title": item?.title,
+                        "label": item?.label,
+                        "likes": item?.likes,
+                        "img": item?.img,
+                        "tag": item?.tag,
+                        "price": realOptions[0]["price"],
+                        "discountPrice": realOptions[0]["discountPrice"]
+                    }
+                    return itemInfo
+                }
+            })
+            // promise형식을 object 형식으로..
+            let infoJson: Array<object | null> = []
+            // itemInfo의 개수
+            let cnt = 0
+            // 추가되는 index
+            let index = 0
+            while (cnt < infoPromise.length) {
+                let info = await infoPromise[cnt]
+                // 빈값({})이 아니면
+                if (!_.isEmpty(info)) {
+                    // index에 info 넣기
+                    infoJson[index] = info
+                    index++
+                    cnt++
+                } else {
+                    cnt++
+                }
+            }
+            return infoJson
+        }
+    }
+    catch (error) {
+        console.log("referenceOfExhibition" + error.message)
+        return 0
+    }
+}
 
 
 
